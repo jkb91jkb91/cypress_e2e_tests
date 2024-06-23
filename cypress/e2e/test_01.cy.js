e9063fbc385adc4c6a86924caa578804c9445eff
jkb91jkb91 <32479224+jkb91jkb91@users.noreply.github.com>
2024-06-23 14:56:24 +0200

    Update test_01.cy.js

--- cypress/e2e/test_01.cy.js
+++ cypress/e2e/test_01.cy.js
@@ -1,7 +1,7 @@
 describe('Training Page Test', () => {
     it('should display the text "Please select a training"', () => {
       cy.visit('http://localhost:80')
-      cy.contains('Plsease select a training')
+      cy.contains('Please select a training')
         .should('be.visible')
     })
   })
